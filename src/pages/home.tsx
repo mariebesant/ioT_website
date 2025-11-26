8e15d6151d25dc87652fbe6e73f24d6ca498b67c
mariebesant <mariebesant02@gmail.com>
2025-11-26 15:16:05 +0100

    initial

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,16 +1,20 @@
 
-import InfoBox from '../components/card'
+import InfoBox from '../components/InfoBox'
 import './home.css'
+import garageClosed from '../assets/garage/garage_geschlossen.png'
+import garageLeftOpen from '../assets/garage/garage_offen_links.png'
+import garageRightOpen from '../assets/garage/garage_offen_rechts.png'
+import garageOpen from '../assets/garage/garage_offen_zwei.png'
+
 
 function Home() {
     
 
     return(
         <>
-            <InfoBox image='s' description='k' title='k'></InfoBox>
+            <InfoBox image={garageClosed} description='k' title='c'></InfoBox>
         </>
     )
 }
 
 export default Home
-
